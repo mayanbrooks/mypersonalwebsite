7d093b7e7b96d40825ac4cf9aee05b69499f09cc
Maya Brooks <maya.n.brooks@gmail.com>
2020-10-25 13:33:02 -0400

    Add files via upload
    2020 updates to assets, resume, videos, etc.

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
     $('.collapsible').collapsible();
   });
+
+
+  var app = document.getElementById('nametype');
+
+  var typewriter = new Typewriter(app, {
+      loop: true
+  });
+
+  typewriter.typeString('Hi, I\'m Maya Brooks!')
+  .pauseFor(2500)
+   .deleteAll()
+   .typeString('Great to meet you!')
+   .pauseFor(2500)
+   .deleteAll()
+   .start();
